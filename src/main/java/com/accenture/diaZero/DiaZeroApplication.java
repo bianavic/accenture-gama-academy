64c9e2ca285c7b71cc733fdb69cac520c2f490b2
bianavic <bianavic@gmail.com>
2020-10-16 16:54:19 -0300

    exercicios dia zero completos

---
+++ src/main/java/com/accenture/diaZero/DiaZeroApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.diaZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaZeroApplication.class, args);
+	}
+
+}
